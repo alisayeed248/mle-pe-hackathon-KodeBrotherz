b0727852c12a0b34be94cd5732e3b3d5fabf7bc3
Naimul Islam <naislamul@gmail.com>
2026-04-04 16:18:49 -0400

    feat: update locust to test Redis cache hits on redirects

--- locustfile.py
+++ locustfile.py
@@ -1,13 +1,27 @@
 from locust import HttpUser, task, between
 
 class URLShortenerUser(HttpUser):
-    wait_time = between(1, 2)
+    wait_time = between(0.5, 1)
+    short_codes = []
 
-    @task(3)
+    def on_start(self):
+        # Create a URL to redirect to during the test
+        res = self.client.post("/shorten", json={"url": "https://example.com"})
+        if res.status_code == 201:
+            self.short_codes.append(res.json()["short_code"])
+
+    @task(1)
     def shorten_url(self):
-        self.client.post("/shorten", json={
-            "url": "https://example.com/test"
-        })
+        res = self.client.post("/shorten", json={"url": "https://example.com"})
+        if res.status_code == 201:
+            self.short_codes.append(res.json()["short_code"])
+
+    @task(5)
+    def redirect_url(self):
+        # Hit cached redirects - this is where Redis shines
+        if self.short_codes:
+            code = self.short_codes[0]  # hit same URL repeatedly = cache hits
+            self.client.get(f"/{code}", allow_redirects=False)
 
     @task(1)
     def health_check(self):
